49528547ba92fd4ecdd88d259f0a8815e9ff9823
Yohannes Haile <yohannesakhaile@gmail.com>
2024-09-16 14:06:14 +0300

    Initial Commit

---
+++ TryChapa4iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TryChapa4iOS
+//
+//  Created by Yohannes Aklilu Haile on 9/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
